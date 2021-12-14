da8201ab56da9328ad18e128dcd9570a3734044d
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-14 21:00:06 +1030

    upload

--- aff_a.c
+++ aff_a.c
@@ -5,20 +5,23 @@ int		main(int argc, char **argv)
 	int i;
 
 	i = 0;
-	if (argc == 2)
-	{
-		while (argv[1][i] != '\0')
+    if (argc == 0)
+    {
+	    write(1, "a\n", 2);
+    }
+	else
+    {
+        while (argv[1][i])
 		{
 			if (argv[1][i] == 'a')
 			{
 				write(1, "a\n", 2);
 				return (0);
 			}
-			i++;
+			i += 1;
 		}
 		write(1, "\n", 1);
 		return (0);
 	}
-	write(1, "a\n", 2);
 	return (0);
 }
