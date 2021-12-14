0974eacce1f95268822843f5e6067acc290d9987
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-14 20:22:45 +1030

    init upload

---
+++ maff_revalpha.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void    ft_maff_revalpha(char c)
+{   
+    int count;            
+    
+    while(c >= 'a')    
+    {        
+        if(count % 2 == 0)
+        {   
+           c -= 32;  
+           write(1, &c, 1); 
+           c += 32;                                 
+       } 
+       else{            
+           write(1, &c, 1);
+       }       
+       c--; 
+       count++;                
+    }
+    write(1, "\n", 1);
+}
+
+int main()
+{
+    ft_maff_revalpha('z');
+    return 0;
+}
