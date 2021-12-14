3a17680916ef6b891a6d7b6abb40ee35a64b793f
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-14 21:38:51 +1030

    upload

---
+++ fizzbuzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_write_number(int num)
+{
+    char c;
+
+	if (num >= 10)
+    {
+		ft_write_number(num / 10);
+    }
+    c = (num % 10) + '0';
+	write(1, &c, 1);
+}
+int main()
+{
+    
+    int num = 1;
+    
+    while (num <= 100)
+    {
+        if (num % 3 == 0 && num % 5 == 0)
+        {
+            write(1, "fizzbuzz", 8);            
+        }
+        else if (num % 3 == 0)
+        {
+            write(1, "fizz", 4);            
+        }
+        else if (num % 5 == 0)
+        {
+            write(1, "buzz", 4);            
+        }
+        else{
+            ft_write_number(num);
+        }
+        write(1, "\n", 1);
+        num++;
+    }
+    return 0;
+}
