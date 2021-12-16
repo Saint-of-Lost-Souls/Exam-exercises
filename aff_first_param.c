b195bae118b88266ee534a06adcd7103f1a9082a
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-16 15:01:21 +1030

    update

--- aff_first_param.c
+++ aff_first_param.c
@@ -9,7 +9,7 @@ int		main(int argc, char **argv)
 {
 	int i;
 
-	i = -1;
+	i = 0;
 	if (argc > 1)
     {        
 		while (argv[1][i])
@@ -17,6 +17,7 @@ int		main(int argc, char **argv)
 			write(1, &argv[1][i], 1);
 			i++;
         }
+	}
 	write(1, "\n", 1);
 	return (0);
 }
